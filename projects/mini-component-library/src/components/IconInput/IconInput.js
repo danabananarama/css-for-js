ab1981e2ed7fa15ef2bbc67a04c7c4ba4e0ff448
Dana Ma <dma@zendesk.com>
2021-10-18 21:52:29 +1100

    Module 3: IconInput attempt
    Much more accessible than the last one (which was really hard, lots of
    things I would never have thought of without the video).
    
    I had to cheat a bit to ge tthings like "inherit" and also make sure we
    were using the native input and not just making our own.

--- projects/mini-component-library/src/components/IconInput/IconInput.js
+++ projects/mini-component-library/src/components/IconInput/IconInput.js
@@ -6,6 +6,17 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const sizes = {
+  small: {
+    fontSize: 14,
+    iconSize: 16
+  },
+  large: {
+    fontSize: 18,
+    iconSize: 20
+  }
+}
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +24,50 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const {fontSize, iconSize} = sizes[size];
+  return <Wrapper>
+    <IconWrapper>
+      <Icon id={icon} size={iconSize} />
+    </IconWrapper>
+    <VisuallyHidden>
+      <label>{label}</label>
+    </VisuallyHidden>
+    <Input placeholder="Search..." fontSize={fontSize} iconSize={iconSize} style={{"--width": `${width}px`}} />
+    </Wrapper>;
 };
+
+const Input = styled.input`
+  font-size: ${p => p.fontSize/16}rem;
+  border: none;
+  width: var(--width);
+  font-family: Roboto;
+  padding-left: ${p => 1.5*p.iconSize}px;
+  margin-bottom: 4px;
+  /* Use parent style rather than user agent stylesheet */
+  color: inherit;
+  font-weight: 700;
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+`
+
+const Wrapper =  styled.div`
+  border-bottom: ${COLORS.black} 1px solid;
+  max-width: max-content;
+
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`
+
+const IconWrapper = styled.div`
+  position: absolute;
+  pointer-events: none;
+`
+
 
 export default IconInput;
